ec9f1c630ba69d3534f94fec7e7c67b125eea21b
medab2 <med.abou.ouf55@gmail.com>
2021-10-09 12:14:52 +0100

    TP1

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("debug", "debug");
+        Log.e("error","error");
+        Log.v("verbose","verbose");
+        Log.i("info ","info");
+        Log.w("warn","warn");
+
+
+    }
+}
